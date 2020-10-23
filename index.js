2b334551c79b49b1a3a8d664b64c7a6680b00364
abhinavbarve <abhinav.barve2001@gmail.com>
2020-10-23 19:27:53 +0530

    v1 complete

--- index.js
+++ index.js
@@ -1,109 +1,56 @@
-function start() {
-    console.log("Start")    
+let correct_cases = []
+let cases = 0;
+for (let i = 11; i <= 18; i++) {
+    for (let j = 21; j <= 28; j++) {
+        if (checkVal(i, j) === false) { continue; };
+        for (let k = 31; k <= 38; k++) {
+            if (checkVal(i, j, k) === false) { continue; };
+            for (let l = 41; l <= 48; l++) {
+                if (checkVal(i, j, k, l) === false) { continue; };
+                for (let m = 51; m <= 58; m++) {
+                    if (checkVal(i, j, k, l, m) === false) { continue; };
+                    for (let n = 61; n <= 68; n++) {
+                        if (checkVal(i, j, k, l, m, n) === false) { continue; };
+                        for (let o = 71; o <= 78; o++) {
+                            if (checkVal(i, j, k, l, m, n, o) === false) { continue; };
+                            for (let p = 81; p <= 88; p++) {
+                                if (checkVal(i, j, k, l, m, n, o, p) === false) { continue; };
+                                correct_cases.push([i, j, k, l, m, n, o, p]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
+// check validity of any combination;
+function checkVal() {
+    let all = [...arguments];
+    for (let i = 0; i < [...arguments].length; i++) {
+        for (let j = 0; j < [...arguments].length; j++) {
+            if (all[i] !== all[j]) {
+                if ((flr(all[i] / 10) === flr(all[j] / 10)) || (all[i] % 10 === all[j] % 10) || Math.abs((flr(all[i] / 10) - flr(all[j] / 10)) / (all[i] % 10 - all[j] % 10)) === 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true
+}
+function flr(x) { return Math.floor(x) };
 
-
-// k = 1
-
-// # A utility function to print solution  
-// def printSolution(board):  
-  
-//     global k 
-//     print(k, "-\n") 
-//     k = k + 1
-//     for i in range(4):  
-//         for j in range(4): 
-//             print(board[i][j], end = " ") 
-//         print("\n") 
-//     print("\n")  
-  
-// ''' A utility function to check if a queen can  
-// be placed on board[row][col]. Note that this  
-// function is called when "col" queens are  
-// already placed in columns from 0 to col -1.  
-// So we need to check only left side for  
-// attacking queens '''
-// def isSafe(board, row, col) :
-      
-//     # Check this row on left side  
-//     for i in range(col):  
-//         if (board[row][i]):  
-//             return False
-  
-//     # Check upper diagonal on left side  
-//     i = row 
-//     j = col 
-//     while i >= 0 and j >= 0: 
-//         if(board[i][j]): 
-//             return False; 
-//         i -= 1
-//         j -= 1
-  
-//     # Check lower diagonal on left side  
-//     i = row 
-//     j = col 
-//     while j >= 0 and i < 4: 
-//         if(board[i][j]): 
-//             return False
-//         i = i + 1
-//         j = j - 1
-  
-//     return True
-  
-// ''' A recursive utility function to solve N  
-// Queen problem '''
-// def solveNQUtil(board, col) : 
-      
-//     ''' base case: If all queens are placed  
-//     then return true '''
-//     if (col == 4):  
-//         printSolution(board)  
-//         return True
-  
-//     ''' Consider this column and try placing  
-//     this queen in all rows one by one '''
-//     res = False
-//     for i in range(4): 
-      
-//         ''' Check if queen can be placed on  
-//         board[i][col] '''
-//         if (isSafe(board, i, col)):  
-          
-//             # Place this queen in board[i][col]  
-//             board[i][col] = 1;  
-  
-//             # Make result true if any placement  
-//             # is possible  
-//             res = solveNQUtil(board, col + 1) or res;  
-  
-//             ''' If placing queen in board[i][col]  
-//             doesn't lead to a solution, then  
-//             remove queen from board[i][col] '''
-//             board[i][col] = 0 # BACKTRACK  
-          
-//     ''' If queen can not be place in any row in  
-//         this column col then return false '''
-//     return res 
-  
-// ''' This function solves the N Queen problem using  
-// Backtracking. It mainly uses solveNQUtil() to  
-// solve the problem. It returns false if queens  
-// cannot be placed, otherwise return true and  
-// prints placement of queens in the form of 1s.  
-// Please note that there may be more than one  
-// solutions, this function prints one of the  
-// feasible solutions.'''
-// def solveNQ() : 
-  
-//     board = [[0 for j in range(10)]  
-//                 for i in range(10)] 
-  
-//     if (solveNQUtil(board, 0) == False):  
-      
-//         print("Solution does not exist")  
-//         return
-//     return
-  
-// # Driver Code  
-// solveNQ()  
+// display chessboards
+for (let h = 1; h <= 92; h++){
+    $("#chessboard-wrapper").append(`<table cellspacing=0 id="cb${h}" class='chessboard'></table>`)
+    for (let i = 1; i <= 8; i++) {
+        $(`#cb${h}`).append(`<tr id="cb${h}r${i}" class="cb-row"></tr>`)
+        for (let j = 1; j <= 8; j++) {
+            $(`#cb${h}r${i}`).append(`<td id="cb${h}c${i}${j}" class="cb-cell"></td>`)
+            if (correct_cases[h - 1].includes(Number(`${i}${j}`))) {
+                $(`#cb${h}c${i}${j}`).append(`<img class="queen-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQci7TG_et476Y1_9ARyMz74accEnq_i9HMtA&usqp=CAU" height="100%" width="100%" />`);
+            }
+        }
+    }
+}
